ed589d403d74b02bfae8a34f2b7724f3209a95cd
RameshKrishnan M <93977636+Krishrk3@users.noreply.github.com>
2023-09-07 20:41:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+// navbar section
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleButton = document.getElementById("navbarToggle");
+    const navbar = document.getElementById("lis");
+    
+    toggleButton.addEventListener("click", function () {
+        if (navbar.style.display === "none"|| navbar.style.display === "") {
+            navbar.style.display="flex";
+        } else {
+            navbar.style.display = "";
+        }
+    });
+});
+
+// scroll to navbar places
+function scrolltoAbout(event){
+    event.preventDefault();
+    var aboutsection =document.querySelector('#about-section');
+    aboutsection.scrollIntoView({behavior:'smooth'});
+}
+function scrolltoAbout(event){
+    event.preventDefault();
+    var aboutsection =document.querySelector('#service-section');
+    aboutsection.scrollIntoView({behavior:'smooth'});
+}
+function scrolltoAbout(event){
+    event.preventDefault();
+    var aboutsection =document.querySelector('#whyus-section');
+    aboutsection.scrollIntoView({behavior:'smooth'});
+}
+function scrolltoAbout(event){
+    event.preventDefault();
+    var aboutsection =document.querySelector('#reachus-section');
+    aboutsection.scrollIntoView({behavior:'smooth'});
+}
+
+// button click to top off the page
+
+// window.onscroll = function(){ scrollFunction()};
+
+// function scrollFunction(){
+//     var scrollToTopBtn = document.getElementById("scrollToTopbtn");
+//     if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+//         scrollToTopBtn.style.display="block";
+//     }else{
+//         scrollToTopBtn.style.display="none";
+//     }
+// }
+
+function scrollToTop(){
+    document.documentElement.scrollTop=0;
+    document.body.scrollTop=0;
+}
